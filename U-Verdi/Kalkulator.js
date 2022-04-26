a45a1d7c46b9571f463b89ba65d477530e1e8373
eliasstr <94906818+zillva@users.noreply.github.com>
2022-04-26 23:31:46 +0200

    Kalk working kinda

--- U-Verdi/Kalkulator.js
+++ U-Verdi/Kalkulator.js
@@ -1,5 +1,26 @@
-function kalkulator(){
+function kalkulatorDark(){
     Height = document.getElementById("writeDarkHeight").value;
+    Width = document.getElementById("writeDarkWidth").value;
+
+    Height = Height / 100;
+    Width = Width / 100;
+
+    uVerdiOld = document.getElementById("dropdownDarkUOld").value;
+    uverdiNew = document.getElementById("dropdownDarkUNew").value;
+
+    tempUte = -7;
+    tempInne = document.getElementById("writeDarkTemp").value;
+
+    if(document.getElementById("dropdownDarkLocation").value == "Bergen") {
+        prisStrøm = 155.673
+    }
+    else if(document.getElementById("dropdownDarkLocation").value == "Bodø") {
+        prisStrøm = 31.34
+    };
+
+    saveMoney = ((Height * Width) * (uverdiNew - uVerdiOld) * (tempInne - tempUte)) / 1000 * 182 * prisStrøm;
+
+    document.getElementById("kalkDarkResultBox").innerHTML = saveMoney;
 }
 
 console.log()
